aab00e94466b045dc1d1a1ba6eb88a89bedea0bc
Саша Акопян <akopian-sasha@mail.ru>
2019-04-08 21:08:37 +0300

    Сделана субд для обобщенной бд

---
+++ DnDHelperApp/WholeLogic/Work with data/DBMS.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace DnDHelperApp.WholeLogic.Work_with_data
+{
+    // Database Managment System
+    public class DBMS<T> where T:IComparable<T>, IEnumerable<T>
+    {
+        Dictionary<string, T> database = new Dictionary<string, T>();
+        XmlSerializer formatter = new XmlSerializer(typeof(Dictionary<string, T>));
+        private string databasePath = $@"Database\{typeof(T)}.xml";
+
+        public DBMS()
+        {
+            if (!File.Exists(databasePath))
+            {
+                SaveData();
+            }
+            LoadData();
+        }
+
+        public void AddNewData(string dataName, T dataValue)
+        {
+            database.Add(dataName, dataValue);
+        }
+
+        public void DeleteData(string dataName)
+        {
+            database.Remove(dataName);
+        }
+
+        public T GetData(string dataName)
+        {
+            return database[dataName];
+        }
+
+        public void SaveData()
+        {
+            using (var xmlWriter = XmlWriter.Create(databasePath))
+            {
+                formatter.Serialize(xmlWriter, database);
+                xmlWriter.Close();
+            }
+        }
+
+        public void LoadData()
+        {
+            using (var xmlReader = XmlReader.Create(databasePath))
+            {
+                Dictionary<string, T> database = (Dictionary<string, T>)formatter.Deserialize(xmlReader);
+                xmlReader.Close();
+            }
+        }
+
+        public bool DataExists(string dataName)
+        {
+            if (!database.Any())
+            {
+                LoadData();
+            }
+            return database.ContainsKey(dataName);
+        }
+
+
+        public void ChangeUserData(string dataName,T newData)
+        {
+            database[dataName] = newData;
+        }
+    }
+}
